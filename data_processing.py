56e686d891315d48521970b46670f960d9d56594
Gvanderl <gvanderl@ucsd.edu>
2018-12-02 23:59:20 -0800

    Data Loading and one hot encoding

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,17 @@
+import pandas as pd
+from pathlib import Path
+from gensim.models import word2vec
 
-def load_data():
-    pass
+
+def load_data(fpath = Path(__file__).resolve().parent / 'data' / 'ExtractedTweets.csv', num_rows=0):
+    df = pd.read_csv(fpath.resolve())
+    if num_rows > 0:
+        df = df.sample(n=num_rows)
+    return df
 
 
-def one_hot():
-    pass
+def one_hot(df):
+    return df.get_dummies(' ')
 
 
 def w2v():
