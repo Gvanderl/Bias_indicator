8398a193baba82c5404db2f4e9572d47ecefbf12
ddl012 <ddl012@ucsd.edu>
2018-12-04 18:31:08 -0800

    added knn

--- classification.py
+++ classification.py
@@ -7,7 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, confusion_matrix  
 
 # https://stackabuse.com/k-nearest-neighbors-algorithm-in-python-and-scikit-learn/
-def KNN(x, y, num_ks, test_size=0.20, n_neighbors=5):
+def KNN(x, y, num_ks=10, test_size=0.20, n_neighbors=5):
     # x is nxk array of processed tweet data (n tweets, k features)
     # y is list of n party labels (dem or rep)
     # num_ks: number of k-values to test (see which gets best error rate)
@@ -19,7 +19,7 @@ def KNN(x, y, num_ks, test_size=0.20, n_neighbors=5):
     print(classification_report(y_test, y_pred))
     
     error = []
-    # Calculating error for K values between 1 and 40
+    # Calculating error for K values between 1 and num_ks
     for i in range(1, num_ks):  
         knn = KNeighborsClassifier(n_neighbors=i)
         knn.fit(x_train, y_train)
